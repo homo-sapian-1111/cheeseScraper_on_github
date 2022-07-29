db388de7b552ee8f540bbaa44823e291b70fbc3c
Eiger Li <eigerli@outlook.com>
2022-07-29 11:09:48 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import wikipediaapi
+from colors import *
+
+wiki_wiki = wikipediaapi.Wikipedia('en')
+page_name = "Cheese"
+
+
+page_py = wiki_wiki.page(page_name)
+
+# print Summary section
+if not page_py.exists():
+    raise ValueError("Page does not exist")
+
+summary = page_py.summary.split(".")
+for index in range(5):
+    print(f"{color('[Summary]', fg='green')}  {summary[index]}.")
+
+# new feature: print the second section of the page
+sec2 = page_py.sections[2]   
+sec2_text = sec2.text.split("\n")
+for index in range(5):
+    print(color(f'[{sec2.title}]', fg='blue') + f"  {sec2_text[index]}.")
+
